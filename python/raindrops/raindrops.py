c51ae2a60e314b93fe4701d61c6233c210532c6c
Nicolas Camacho <ncamacho1@cuc.edu.co>
2020-03-14 00:05:29 -0500

    :bug: Fixed raindrops push

--- python/raindrops/raindrops.py
+++ python/raindrops/raindrops.py
@@ -1,2 +1,7 @@
+DROPS = [(3, 'Pling'), (5, 'Plang'), (7, 'Plong')]
+
+
 def convert(number):
-    factors = [(3, 'Pling'), (5, 'Plang'), (7, 'Plong')]
+    return ''.join(
+        drop[1] for drop in DROPS if number % drop[0] == 0
+        ) or str(number)
